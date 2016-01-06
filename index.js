dcdbcacd83c97e9244bd8dee9d064fca9f2a9d16
Mihael Konjevic <konjevic@gmail.com>
2016-01-06 14:28:58 +0100

    Add sponsors part, transitions on slide change

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import initView from "./index.stache!";
 import "can/map/define/";
 import "can/list/promise/";
 import "./style.less!";
+import "sponsors/";
 
 const URL = 'http://bithub.com/api/v3/embeds/143/entities?view=public&tenant_name=calm_dove_6826&image_only=true&offset=0&limit=5';
 
@@ -13,7 +14,7 @@ var Bit = can.Model.extend({
 }, {});
 
 var Hub = can.Component.extend({
-	tag: 'hs-hub',
+	tag: 'hg-hub',
 	template: initView,
 	scope : {
 		currentBitIdx: 0,
@@ -27,6 +28,15 @@ var Hub = can.Component.extend({
 		currentBit : function(){
 			if(this.attr('bits').isResolved()){
 				return this.attr('bits.' + this.attr('currentBitIdx'));
+			}
+		},
+		nextBit : function(){
+			if(this.attr('bits').isResolved()){
+				var nextBitIdx = this.attr('currentBitIdx') + 1;
+				if(nextBitIdx === this.attr('bits').attr('length')){
+					nextBitIdx = 0;
+				}
+				return this.attr('bits.' + nextBitIdx);
 			}
 		}
 	},
@@ -43,19 +53,26 @@ var Hub = can.Component.extend({
 		cycle : function(){
 			var self = this;
 			setTimeout(function(){
-				var currentBitIdx = self.scope.attr('currentBitIdx');
-				var length = self.scope.attr('bits.length');
-				var nextIdx = currentBitIdx + 1;
-				if(nextIdx >= length){
-					nextIdx = 0;
+				if(!self.element){
+					return;
 				}
-				self.scope.attr('currentBitIdx', nextIdx);
-				self.cycle();
+				self.element.find('.current-bit').addClass('current-bit-exiting');
+				self.element.find('.next-bit').addClass('next-bit-entering');
+				setTimeout(function(){
+					var currentBitIdx = self.scope.attr('currentBitIdx');
+					var length = self.scope.attr('bits.length');
+					var nextIdx = currentBitIdx + 1;
+					if(nextIdx >= length){
+						nextIdx = 0;
+					}
+					self.scope.attr('currentBitIdx', nextIdx);
+					self.cycle();	
+				}, 600);				
 			}, 5000);
 		}
 	}
 });
 
-var template = stache("<hs-hub></hs-hub>");
+var template = stache("<hg-hub></hg-hub>");
 
 $('#app').html(template());
